704d1914a60358c56efb7837161e89973b3f3ede
sumaiyakhondoker <sumaiyakhondoker85@gmail.com>
2024-09-06 19:40:54 +0600

    tailwind setup complete in knowledge cafe

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-4xl'>Knowledge Cafe</h1>
+            {/* img link: https://i.ibb.co/jMd1nWg/blond-business-woman-standing-with-crossed-arms.jpg */}
+        </div>
+    );
+};
+
+export default Header;
